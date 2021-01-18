008f0aa296859f265b686639adbbe80b7aeb1e77
LachlynR <lachyreynolds@outlook.com>
2021-01-18 14:54:29 +1100

    home page complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import HomePage from "./components/HomePage/HomePage";
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <>
+      <Router>
+        <Route path="/" exact render={(props) => <HomePage { ...props } />} />
+        {/* <Route path="/order" render={props => <OrderPage { ...props } />}  /> */}
+      </Router>
+    </>
   );
 };
 
